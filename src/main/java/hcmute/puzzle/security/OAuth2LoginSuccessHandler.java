e4c71273b6dd612f2bd4d453e8a16d49eb3f3527
Hao Vu <caihoncuagiamnguc@gmail.com>
2022-12-21 06:19:17 +0700

    complete api login with google

--- src/main/java/hcmute/puzzle/security/OAuth2LoginSuccessHandler.java
+++ src/main/java/hcmute/puzzle/security/OAuth2LoginSuccessHandler.java
@@ -21,25 +21,25 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
   public void onAuthenticationSuccess(
       HttpServletRequest request, HttpServletResponse response, Authentication authentication)
       throws IOException, ServletException {
-    CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-    String email = oAuth2User.getEmail();
-
-    System.out.println("Customer's email: " + email);
-
-    String token = userService.processOAuthPostLogin(oAuth2User.getEmail(), oAuth2User.getName());
-
-    System.out.println(token);
-
-    response.addHeader("Authorization", token);
-
-    ResponseObject responseObject = new ResponseObject("401", 200, token);
-    // System.out.println(authException.getMessage());
-    responseObject.setMessage("Invalid token");
-    response.setStatus(200);
-    response.setContentType("application/json");
-
-    ObjectMapper objectMapper = new ObjectMapper();
-    response.getWriter().write(objectMapper.writeValueAsString(responseObject));
+//    CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+//    String email = oAuth2User.getEmail();
+//
+//    System.out.println("Customer's email: " + email);
+//
+//    String token = userService.processOAuthPostLogin(oAuth2User.getEmail(), oAuth2User.getName());
+//
+//    System.out.println(token);
+//
+//    response.addHeader("Authorization", token);
+//
+//    ResponseObject responseObject = new ResponseObject("401", 200, token);
+//    // System.out.println(authException.getMessage());
+//    responseObject.setMessage("Invalid token");
+//    response.setStatus(200);
+//    response.setContentType("application/json");
+//
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    response.getWriter().write(objectMapper.writeValueAsString(responseObject));
 
     super.onAuthenticationSuccess(request, response, authentication);
   }
