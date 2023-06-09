3cb398b27cff227a451d1ab163325969793c4628
Hao Vu <caihoncuagiamnguc@gmail.com>
2023-06-09 16:19:52 +0700

    clean import to fix error build heroku

--- src/main/java/hcmute/puzzle/infrastructure/entities/JobPost.java
+++ src/main/java/hcmute/puzzle/infrastructure/entities/JobPost.java
@@ -1,8 +1,6 @@
 package hcmute.puzzle.infrastructure.entities;
 
 import lombok.*;
-import org.joda.time.DateTime;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
