d1fc536458e3f05693059d803506b5f96ec102d6
Hao Vu <caihoncuagiamnguc@gmail.com>
2023-06-16 16:58:12 +0700

    fix get all blog category

--- src/main/java/hcmute/puzzle/infrastructure/repository/BlogCategoryRepository.java
+++ src/main/java/hcmute/puzzle/infrastructure/repository/BlogCategoryRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface BlogCategoryRepository extends JpaRepository<BlogCategory, Long> {
 
-	@Query(value = "SELECT bc, COUNT(bp) FROM BlogCategory bc JOIN BlogPost bp ON bc.id = bp.blogCategory.id GROUP BY bc ORDER BY COUNT(bp)")
+	@Query(value = "SELECT bc, COUNT(bp) FROM BlogCategory bc LEFT JOIN BlogPost bp ON bc.id = bp.blogCategory.id GROUP BY bc ORDER BY COUNT(bp)")
 	public List<Object[]> getBlogCategoriesWithAmountBlogPost();
 }
